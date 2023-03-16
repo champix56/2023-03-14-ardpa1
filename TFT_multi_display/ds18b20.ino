57f5b6b986ada6445fa3a2a198b3bda358c88dd5
champix56 <desorbaix@free.fr>
2023-03-16 17:56:52 +0100

    code du ds18b20

---
+++ TFT_multi_display/ds18b20.ino
@@ -0,0 +1,22 @@
+#ifdef DS18B20
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 6
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+void setupTemperature() {
+  sensors.begin();
+}
+float readTemperature() {
+  sensors.requestTemperatures();
+  sensorsValues.temperature = sensors.getTempCByIndex(0);
+  return sensorsValues.temperatures;
+}
+#endif
