fe1c0acc7758db3781b7a26efdd97d8bb8da7ee6
champix56 <desorbaix@free.fr>
2023-03-16 18:04:42 +0100

    add comment

--- TFT_multi_display/config.h
+++ TFT_multi_display/config.h
@@ -1,13 +1,21 @@
 #ifndef _CONFIG_H_
 
 #define _CONFIG_H_
+  /***
+  PRESSURE
+  ***/
   #define BMP180
-  
+  /***
+  TEMPERATURE
+  ***/
   //#define BMP180_TEMPERATURE
   //#define DUMMY_TEMPERATURE
   #define DS18B20_TEMPERATURE
-
+ /***
+ OUTPUT
+ ***/
   #define ST7735
 
+//check config for multiple sensor type defined
   #include "config_adv.h"
 #endif
