ee4f435e5e5a0d3a6875076ebe9deb1cef32baaa
champix56 <desorbaix@free.fr>
2023-03-17 16:11:58 +0100

    gestion union

--- union/struct.h
+++ union/struct.h
@@ -1,7 +1,11 @@
 struct Datas {
   uint8_t humidity;
-  float temperature; 
+  float temperature;
   char message[20];
+};
+union U_Datas {
+  struct Datas datas;
+  uint8_t data8[sizeof(struct Datas)];
 };
 //#define EMITTER
 #define RECIEVER
--- union/union.ino
+++ union/union.ino
@@ -1,5 +1,5 @@
 #include "struct.h"
-struct Datas data;
+struct Datas sdata;
 void setup() {
   Serial.begin(9600);
 // put your setup code here, to run once:
@@ -20,12 +20,41 @@ void loop() {
 }
 #ifdef EMITTER
 void setupValues() {
+  sdata.humidity = 70;
+  sdata.temperature = 26.2F;
+  strcpy(sdata.message , "Hello de lu");
 }
 
 void emit() {
+  while (!Serial.available()) { delay(50); }
+  char c = Serial.read();
+  if (c == 'o') {
+    U_Datas ud;
+    ud.data = sdata;
+    for (int = 0; i < sizeof(struct Datas); i++) {
+      Serial.write(ud.data8[i]);
+    }
+    delay(1000);
+  }
+  Serial.flush();
 }
 #endif
 #ifdef RECIEVER
 void recieve() {
+  //while(Serial.available()){Serial.read();}
+  //delay(1000);
+  Serial.write('o');
+  uint8_t i=0;
+  U_Datas ud;
+  while(Serial.available() && i<sizeof(struct Datas))
+  {
+    ud.data8[i]=Serial.read();
+    i++;
+  }
+  Serial.print("humidité:");
+  Serial.println(ud.datas.humidity);
+  Serial.print("humidité:");
+  Serial.println(ud.datas.temperature);
+  Serial.println(ud.datas.message);
 }
 #endif
